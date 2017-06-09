c1d00aec64e3bb03c4f78cc61222823b85967261
ZZZero-mba <914426031@qq.com>
2017-06-09 16:19:24 +0800

    no message

--- server/history_chat.js
+++ server/history_chat.js
@@ -5,10 +5,20 @@ const RDS_OPTS = {}
 
 let client = redis.createClient(RDS_PORT, RDS_HOST, RDS_OPTS)
 
+let redisReady = false
 client.on('end', function(err) {
   console.log('end')
 })
 
 client.on('connect', function () {
-
+  redisReady = true
 })
+
+module.export = {
+  setHistory: function () {
+    client.set('historyChat')
+  },
+  getHistory: function () {
+    client.get('historyChat')
+  }
+}
